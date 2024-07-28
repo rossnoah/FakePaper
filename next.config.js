f0851e160c7e752be5a2c01950ac002752d50fc0
Noah Ross <me@noah.dev>
2024-07-28 08:18:50 -0700

    add analytics

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ module.exports = {
         source: "/storage/:path*",
         destination: process.env.BLOB_URL + "/:path*",
       },
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
     ];
   },
 };
