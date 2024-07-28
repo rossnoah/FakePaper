f80682ca50f12cc60003151fba6dc9639225bbe6
Noah Ross <me@noah.dev>
2024-07-28 08:14:45 -0700

    oops fix rewrite

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: "/storage/:path*",
-        destination: process.env.BLOB_URL + "/storage/:path*",
+        destination: process.env.BLOB_URL + "/:path*",
       },
     ];
   },
