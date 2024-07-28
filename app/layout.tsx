eb1d7bac7e8872dc0d164cac46fa9d6ef441255d
Noah Ross <me@noah.dev>
2024-07-28 09:22:11 -0700

    analytics fix ig

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { Shell } from "@/components/shell";
 import { Toaster } from "@/components/ui/toaster";
 import NonSsr from "@/components/non-ssr";
+import { CSPostHogProvider } from "./providers";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -23,15 +24,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        <Shell>{children}</Shell>
-        <Toaster />
-      </body>
+      <CSPostHogProvider>
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <Shell>{children}</Shell>
+          <Toaster />
+        </body>
+      </CSPostHogProvider>
     </html>
   );
 }
