aaf0d951bbeadd0befa81998ba895b3a8f56e47e
Noah Ross <noahwross@gmail.com>
2024-08-30 00:48:42 -0400

    move buy coffee button

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,17 +69,18 @@ export default function RootLayout({
 
         {/* eslint-disable-next-line @next/next/no-sync-scripts */}
         <script
-          data-name="BMC-Widget"
-          data-cfasync="false"
-          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-          data-id="noahross"
-          data-description="Support me on Buy me a coffee!"
-          data-message=""
-          data-color="#FFDD00"
-          data-position="Right"
-          data-x_margin="18"
-          data-y_margin="18"
-        ></script>
+  data-name="BMC-Widget"
+  data-cfasync="false"
+  src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+  data-id="noahross"
+  data-description="Support me on Buy me a coffee!"
+  data-message=""
+  data-color="#FFDD00"
+  data-position="Left"
+  data-x_margin="18"
+  data-y_margin="18"
+  style="bottom: 18px; left: 18px; position: fixed;"
+></script>
       </head>
       <CSPostHogProvider>
         <body
