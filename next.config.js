1f978abb30c5d520af289bee9f9f0b959890ae42
Noah Ross <me@noah.dev>
2024-09-04 10:33:05 -0400

    add s3 rewrite

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ module.exports = {
         destination: process.env.BLOB_URL + "/:path*",
       },
       {
+        source: "/s3/:path*",
+        destination: process.env.S3_URL + "/:path*",
+      },
+      {
         source: "/ingest/static/:path*",
         destination: "https://us-assets.i.posthog.com/static/:path*",
       },
